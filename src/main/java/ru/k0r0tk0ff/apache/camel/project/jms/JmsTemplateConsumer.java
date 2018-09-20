3b919aa16268f960e4dd59a1faaac8705d73126e
Alexandr <korotkov34reg@gmail.com>
2018-09-20 09:16:59 +0300

    Create JmsTemplateConsumer.java

---
+++ src/main/java/ru/k0r0tk0ff/apache/camel/project/jms/JmsTemplateConsumer.java
@@ -0,0 +1,39 @@
+package ru.k0r0tk0ff.apache.camel.project.jms;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import ru.k0r0tk0ff.apache.camel.project.db.JmsRepository;
+
+import javax.jms.TextMessage;
+
+
+@Component
+public class JmsTemplateConsumer {
+
+    private static Logger LOGGER =
+            LogManager.getLogger(JmsTemplateConsumer.class);
+
+    @Autowired
+    private JmsRepository jmsRepository;
+
+    @JmsListener(destination = "empty")
+    public void receiveMessage(TextMessage textMessage) {
+        try {
+            jmsRepository.writeMessageToDb(textMessage);
+            StringBuilder builder = new StringBuilder();
+            builder.append("Received: <body = ");
+            builder.append(textMessage.getText());
+            builder.append("; header = ");
+            builder.append(textMessage.getJMSType());
+            builder.append(";>");
+            LOGGER.info(builder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("Error in JmsTemplateConsumer class!!", e);
+        }
+    }
+}
+
