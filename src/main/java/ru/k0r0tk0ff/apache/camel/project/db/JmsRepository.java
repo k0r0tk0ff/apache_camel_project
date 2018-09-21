c7b27c807a3f21905f111bf39ffb8f675a72ca70
Alexandr <korotkov34reg@gmail.com>
2018-09-21 13:15:24 +0300

    Update JmsRepository.java

--- src/main/java/ru/k0r0tk0ff/apache/camel/project/db/JmsRepository.java
+++ src/main/java/ru/k0r0tk0ff/apache/camel/project/db/JmsRepository.java
@@ -1,18 +1,32 @@
 package ru.k0r0tk0ff.apache.camel.project.db;
 
+import org.apache.camel.Handler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.lob.DefaultLobHandler;
+import org.springframework.jdbc.support.lob.LobHandler;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.SerializationUtils;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
+import java.io.*;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JmsRepository {
@@ -23,7 +37,8 @@ public class JmsRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void writeMessageToDb(TextMessage textMessage) {
+    @Handler
+    public void writeMessageToDb(InputStream inputStream) {
         String sql = "INSERT into msg (message) values (?)";
         KeyHolder holder = new GeneratedKeyHolder();
 
@@ -32,18 +47,14 @@ public class JmsRepository {
                     sql,
                     Statement.RETURN_GENERATED_KEYS);
             try {
-                ps.setString(1, textMessage.getText());
+                ps.setBlob(1, inputStream);
             } catch (Exception e) {
                 LOGGER.error("Error (insert message body) in JmsRepository class!", e);
             }
             return ps;
         }, holder);
 
-        try {
-            insertHeader(holder.getKey().intValue(), textMessage.getJMSType());
-        } catch (JMSException e) {
-            LOGGER.error("Error insertHeader() in JmsRepository class!", e);
-        }
+            insertHeader(holder.getKey().intValue(), "customType");
     }
 
     private void insertHeader(int recordId, String value) {
@@ -52,14 +63,37 @@ public class JmsRepository {
     }
 
     public void showDataInDb(){
-        System.out.println("------------- Show result in database");
-        String sql = "SELECT msg.message, headers.head " +
+        LOGGER.info("------------- Show result in database");
+        String sql = "SELECT headers.head, msg.message " +
                 " FROM msg" +
                 " JOIN headers" +
                 " ON msg.msg_id = headers.headers_id;";
-        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
-        while(rowSet.next()) {
-            System.out.println(rowSet.getString(1) + "  " + rowSet.getString(2));
+
+        //        byte[] data = SerializationUtils.serialize(entry.getValue());
+        List<Map<String, byte[]>> list = jdbcTemplate.query(
+                sql, new ResultSetExtractor<List<Map<String, byte[]>>>() {
+            @Override
+            public List<Map<String, byte[]>> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                List<Map<String, byte[]>> list = new ArrayList<>();
+
+                while (resultSet.next()) {
+                    Map<String, byte[]> map = new HashMap<String, byte[]>();
+                    map.put(
+                            resultSet.getString(1),
+                            resultSet.getBytes(2));
+                    list.add(map);
+                }
+                return list;
+            }
+        });
+
+        for (Map<String, byte[]> map : list) {
+            for (Map.Entry<String, byte[]> entry : map.entrySet()) {
+                String str = new String(entry.getValue());
+                LOGGER.info(
+                        String.format(
+                                "type: %s  body: %s", entry.getKey(),str));
+            }
         }
     }
 }
