b20ae09700ec9fd4bd5ada8bcb5227582f8d4b7d
Alexandr <korotkov34reg@gmail.com>
2018-09-21 13:31:03 +0300

    Update Application.java

--- src/main/java/ru/k0r0tk0ff/apache/camel/project/Application.java
+++ src/main/java/ru/k0r0tk0ff/apache/camel/project/Application.java
@@ -1,7 +1,5 @@
 package ru.k0r0tk0ff.apache.camel.project;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.CamelContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import ru.k0r0tk0ff.apache.camel.project.db.JmsRepository;
-
-import org.apache.camel.component.jms.JmsComponent;
-import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 @SpringBootApplication(scanBasePackages = "ru.k0r0tk0ff.apache.camel.project")
-public class Application extends CamelConfiguration implements ApplicationRunner {
+public class Application implements ApplicationRunner {
 
     private static Logger LOGGER = LogManager.getLogger(Application.class);
 
@@ -34,17 +29,5 @@ public class Application extends CamelConfiguration implements ApplicationRunner
         TimeUnit.SECONDS.sleep(3);
         jmsRepository.showDataInDb();
         System.exit(0);
-    }
-
-    @Override
-    protected void setupCamelContext(CamelContext camelContext) throws Exception {
-        // setup the ActiveMQ component
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL("vm://localhost?broker.persistent=false&broker.useJmx=false");
-
-        // and register it into the CamelContext
-        JmsComponent answer = new JmsComponent();
-        answer.setConnectionFactory(connectionFactory);
-        camelContext.addComponent("jms", answer);
     }
 }
