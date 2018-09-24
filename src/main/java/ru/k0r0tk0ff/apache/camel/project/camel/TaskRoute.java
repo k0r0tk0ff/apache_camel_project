1b607a36ed5bed5e55c54e9f1bbe2892d506ef7f
Alexandr <korotkov34reg@gmail.com>
2018-09-24 17:12:00 +0300

    Update TaskRoute.java

--- src/main/java/ru/k0r0tk0ff/apache/camel/project/camel/TaskRoute.java
+++ src/main/java/ru/k0r0tk0ff/apache/camel/project/camel/TaskRoute.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.k0r0tk0ff.apache.camel.project.db.JmsRepository;
 
-
 @Component
 public class TaskRoute extends RouteBuilder {
 
     @Autowired
     private ErrorProcessor errorProcessor;
+
+    @Autowired
+    private CountProcessor countProcessor;
 
     @Override
     public void configure() throws Exception {
@@ -19,11 +21,18 @@ public class TaskRoute extends RouteBuilder {
         .choice()
         .when(header("CamelFileName").endsWith(".xml"))
             .to("jms:queue:empty")
+            .process(countProcessor)
         .when(header("CamelFileName").endsWith(".txt"))
             .bean(JmsRepository.class, "writeMessageToDb")
             .to("jms:queue:empty")
+            .process(countProcessor)
         .otherwise()
+            .process(countProcessor)
+            .throwException(new Exception(" Wrong extension !"))
             .to("jms:queue:invalid-queue")
-            .process(errorProcessor);
+        .end()
+            .choice()
+            .when(header("to").isNotNull())
+            .to("smtp://localhost");
     }
 }
