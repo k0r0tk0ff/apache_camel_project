320e1e0929d623b5928aaf4f4cbe9487a3adab7b
Alexandr <korotkov34reg@gmail.com>
2018-09-26 20:14:30 +0300

    Update CamelConfigurationImpl.java

--- src/main/java/ru/k0r0tk0ff/apache/camel/project/camel/CamelConfigurationImpl.java
+++ src/main/java/ru/k0r0tk0ff/apache/camel/project/camel/CamelConfigurationImpl.java
@@ -1,6 +1,6 @@
 package ru.k0r0tk0ff.apache.camel.project.camel;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQXAConnectionFactory;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +10,8 @@ import org.apache.camel.CamelContext;
 public class CamelConfigurationImpl extends CamelConfiguration {
     @Override
     protected void setupCamelContext(CamelContext camelContext) throws Exception {
-        // setup the ActiveMQ component
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL("vm://localhost?broker.persistent=false&broker.useJmx=false");
-
-        // and register it into the CamelContext
+        ActiveMQXAConnectionFactory connectionFactory = new ActiveMQXAConnectionFactory();
+        connectionFactory.setBrokerURL("vm://localhost?broker.persistent=false&broker.useJmx=false&broker.useShutdownHook=false");
         JmsComponent answer = new JmsComponent();
         answer.setConnectionFactory(connectionFactory);
         camelContext.addComponent("jms", answer);
